b845ee66058dbe5d8213220b4fe53a78ae4761dc
shivam501-00 <khedekarshivam333@gmail.com>
2024-01-16 00:14:48 +0530

    readjust merge sort code

---
+++ code/Access_modifire.java
@@ -0,0 +1,43 @@
+// import java.util.Scanner;
+
+class Accounts {
+    public String name;
+    private String password;
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Constructor helps to reduce number of statment from main method and main
+    // class.
+
+    Accounts(String name1, String pass1, int x, int y) {
+        System.out.println("Constructor called");
+        this.name = name1;
+        System.out.println(name1);
+
+        // setting pass
+        this.password = pass1;
+        setPass(pass1);
+        System.out.println(getPass());
+        System.out.println(add(x, y));
+    }
+
+    public void setPass(String pass) {
+        this.password = pass;
+    }
+
+    public String getPass() {
+        return this.password;
+    }
+
+}
+
+public class Access_modifire {
+    public static void main(String[] args) {
+
+        Accounts A1 = new Accounts("SHIVAM", "ABDS@123", 5, 6);
+        Accounts A2 = new Accounts("TUSHAR", "AVCDF@23", 10, 20);
+
+    }
+}
--- code/merge_sort.java
+++ code/merge_sort.java
@@ -1,51 +1,89 @@
-// package code;
+package code;
 
+import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class merge_sort {
     public static void main(String[] args) {
-        int[] a = { 2, 1, 8, 5, 7, 3, 9 };
-        int n = a.length;
-        merge_sorting(a, 0, (n - 1));
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter size of array : ");
+        int n = sc.nextInt();
 
-    static void merge_sorting(int[] a, int low, int high) {
-        // base case
-        if (low == high) {
-            return;
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
         }
-        int mid = (low + high) / 2;
-        merge_sorting(a, low, mid);
-        merge_sorting(a, mid + 1, high);
-        merg(a, low, mid, high);
+        System.out.println("Array a before sorting : " + Arrays.toString(a));
+        System.out.println(" ");
+        Sort_merge.merge_sorting(a, 0, n - 1);
+        System.out.println("Array after sorting is :" + Arrays.toString(a));
+        sc.close();
     }
 
-    static void merg(int[] a, int low, int mid, int high) {
+}
+
+/**
+ * Sort
+ */
+class Sort_merge {
+
+    private static void merg(int[] a, int low, int mid, int high) {
         // create new array list to add element after sorting
-        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
         // assign pointers to first element of left and right array
         int l = low;
         int r = mid + 1;
 
+        /*
+         * while element are present in both side sorted array we want to compare
+         * element from both arrays with each other and add which ever element is small
+         * first
+         * NOTE: considering both side arrays are sorted we don't need to check element
+         * in same array.
+         */
         while (l <= mid && r <= high) {
             if (a[l] <= a[r]) {
-                arr.add(a[l]);
+                temp.add(a[l]);
                 l++;
             } else {
-                arr.add(a[r]);
+                temp.add(a[r]);
                 r++;
             }
         }
+        /*
+         * If right side array get exauhsted completely then simply add remaining left
+         * side array element without checking as the are already sorted
+         */
         while (l <= mid) {
-            arr.add(a[l]);
+            temp.add(a[l]);
             l++;
         }
+        // Vice versa for remaining right side element
         while (r <= high) {
-            arr.add(a[r]);
+            temp.add(a[r]);
             r++;
         }
-        for (int i = 0; i < a.length; i++) {
-            a[i] = arr.get(i);
+        /*
+         * All this addintion of element takes place in temp arraylist so add same
+         * ordered sorted element in array
+         */
+        for (int i = low; i <= high; i++) {
+            a[i] = temp.get(i - low);
         }
+    }
+
+    public static void merge_sorting(int[] a, int low, int high) {
+        // base case
+        if (low >= high) {
+            return;
+        }
+        // find out point from which we want to break array
+        int mid = (low + high) / 2;
+        // call same function recursivly for two newly created array with help of mid
+        merge_sorting(a, low, mid);
+        merge_sorting(a, mid + 1, high);
+        // merge array once it get seperated till base case.
+        merg(a, low, mid, high);
     }
 }
