9cf15580b9e62ab8c4de112fd39ed40b3ba6b668
shivam501-00 <khedekarshivam333@gmail.com>
2024-02-19 00:37:56 +0530

    Added better solution for longest sub array with sum k

--- code/Array_problem/longest_subarray.java
+++ code/Array_problem/longest_subarray.java
@@ -1,12 +1,16 @@
 package code.Array_problem;
 
+import java.util.*;
+// import code.datastructures.hashmap;
+
 //longest subarray with sum K with array of positive numbers 
 
 public class longest_subarray {
     public static void main(String[] args) {
         int[] a = { 1, 2, 1, 3, 1, 1, 1, 1, 3 };
         int n = a.length;
-        System.out.println("Longest subarray with sum 3 " + brute_l_sub(a, 4, n));
+        // System.out.println("Longest subarray with sum 3 " + brute_l_sub(a, 4, n));
+        System.out.println("Longest subarray with sum 3 " + better_l_sub(a, 4, n));
 
     }
     // brute approach
@@ -28,6 +32,43 @@ public class longest_subarray {
             }
         }
         return len;
+    }
+
+    static int better_l_sub(int[] a, long k, int n) {
+        long sum = 0;
+        int maxlen = 0;
+
+        Map<Long, Integer> preSum = new HashMap<>();
+        for (int i = 0; i < a.length; i++) {
+            /* First we calculate sum till i */
+            sum += a[i];
+            /*
+             * check if sum till i is equal to sum we want i.e. k, if yes then update maxlen
+             * by checking whoever have bigger length
+             */
+            if (sum == k) {
+                maxlen = Math.max(maxlen, i + 1);
+            }
+
+            /* find remider */
+            long rem = sum - k;
+
+            /*
+             * check if map contains rem at any point and then get len by subtracting its
+             * value which is stored as index in ma p from current index i. and then check
+             * which have bigger length
+             */
+            if (preSum.containsKey(rem)) {
+                int len = i - preSum.get(rem);
+                maxlen = Math.max(maxlen, len);
+            }
+            /* add sum and i as key value in map */
+            if (!preSum.containsKey(sum)) {
+                preSum.put(sum, i);
+            }
+
+        }
+        return maxlen;
     }
 
 }
