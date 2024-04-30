0c1e4826e9a0eca35c52c95b02875c21f7300d4a
shivam501-00 <khedekarshivam333@gmail.com>
2024-05-01 00:44:17 +0530

    zero_ones_twos

--- code/Array_problem/zero_onr_two.java
+++ code/Array_problem/zero_onr_two.java
@@ -7,7 +7,9 @@ public class zero_onr_two {
         int[] nums = { 0, 1, 2, 0, 1, 0, 0, 1, 2, 2, 1, 2, 0, 2 };
         int n = nums.length;
         System.out.println("Sorted array of element 0,1,2 is as follows :");
-        System.out.println(Arrays.toString(better_sol_zero_one_two(nums, n)));
+        // System.out.println(Arrays.toString(better_sol_zero_one_two(nums, n)));
+        System.out.println(Arrays.toString(optimal_sol_zero_one_two(nums, n)));
+
     }
 
     static int[] better_sol_zero_one_two(int[] nums, int n) {
@@ -34,5 +36,40 @@ public class zero_onr_two {
         }
         return nums;
     };
+
+    static int[] optimal_sol_zero_one_two(int[] nums, int n) {
+        int mid = 0;
+        int high = n - 1;
+        int low = 0;
+        /*
+         * -- Consider imaginary array which have value 0 for 0th to [low-1]th index,
+         * have
+         * 1 for [low]to [mid-1] index, have unordered array from [mid] to [high] and
+         * have 2 for [high+1] to n-1 index
+         * -- consider mid and low are both at zero th index at start , going forward we
+         * need to check what value at mid index inarray is stored and accordingly shift
+         * that value to its region and increase mid, low or decrease high accordingly.
+         * -- (VIMP) as per regions we made in 1st point of this intution, low will
+         * always have
+         * 
+         */
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int t = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = t;
+                mid++;
+                low++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else if (nums[mid] == 2) {
+                int t = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = t;
+                high--;
+            }
+        }
+        return nums;
+    }
 
 }
